a97074ab962f682c4bbf24a399fdc3732cc1d7fb
Vincent <overlordnacen@gmail.com>
2019-11-04 11:37:29 +0800

    Add value prop to the input

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,7 +5,7 @@ import "./form-input.styles.scss";
 const FormInput = ({ handleChange, label, ...otherProps }) => {
   return (
     <div className="group">
-      <input type={otherProps.type} required={otherProps.required} name={otherProps.name} className="form-input" onChange={handleChange} />
+      <input type={otherProps.type} value={otherProps.value} required={otherProps.required} name={otherProps.name} className="form-input" onChange={handleChange} />
       {label ? (
         <label
           className={`${otherProps.value.length ? "shrink" : ""} form-input-label`}
