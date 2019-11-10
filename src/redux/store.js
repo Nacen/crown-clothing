d2bd149013d4d3b520392c2a429085da453f7126
Vincent <overlordnacen@gmail.com>
2019-11-10 14:29:50 +0800

    Add redux-logger if environment is development

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,21 @@
-import { createStore, applyMiddleware, compose  } from "redux";
-import { persistStore } from 'redux-persist'
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore } from "redux-persist";
 
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
 // used for redux dev tools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
 export const persistor = persistStore(store);
- 
